20b1775f8d3a96d8ab003a27f8489087fec88e77
Mayank Sharma <87992165+Mynk1@users.noreply.github.com>
2025-12-02 11:08:17 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,40 @@
+function addTodo(){
+
+  const inputJs = document.getElementById('userInput');
+  const listJs = document.getElementById('list');
+
+  if (inputJs.value.trim() === "") return;
+
+  const todo = document.createElement("li");
+
+  todo.innerHTML = `
+    <span class="todo-text">${inputJs.value}</span>
+
+    <div class="actions">
+      <span class="complete-btn action">✅</span>
+      <span class="delete-btn action" onclick="this.parentElement.parentElement.remove()">🗑️</span>
+    </div>
+  `;
+
+  const text = todo.querySelector(".todo-text");
+  const btn = todo.querySelector(".complete-btn");
+
+  // Toggle strike-through on click
+  btn.onclick = function () {
+    if (text.style.textDecoration === "line-through") {
+      text.style.textDecoration = "none";
+      btn.style.opacity = "1";
+      btn.textContent = "✅";
+      btn.style.fontSize = "1rem";    
+    } else {
+      text.style.textDecoration = "line-through";
+      btn.style.opacity = "0.5";
+      btn.style.fontSize = "1.3rem";      
+      btn.textContent = "↺";
+    }
+  };
+
+  listJs.appendChild(todo);
+  inputJs.value = "";
+  inputJs.focus();
+}
